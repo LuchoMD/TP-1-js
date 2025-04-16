69b22bc10e8ae8c24b53bfd6d4a61a146dedef29
Lucho <diaz.luis.10911@gmail.com>
2025-04-16 16:42:46 -0300

    Se realizo el ejercicio extra 1 (pseint)

---
+++ js/extra-1.js
@@ -0,0 +1,8 @@
+/*
+1- Realizar una suma de dos números: Cree un algoritmo que permita al usuario ingresar dos números, realice la suma de ambos y muestre el resultado.
+*/
+let numero1 = parseInt(prompt("Ingrese el primer número:"));
+let numero2 = parseInt(prompt("Ingrese el segundo número:"));
+let suma;
+suma = numero1 + numero2;
+document.writeln("La suma es: ", suma);
