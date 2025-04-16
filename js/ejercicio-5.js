7dec3eecba202570dffabb83cdc411bab19daece
Lucho <diaz.luis.10911@gmail.com>
2025-04-16 15:15:00 -0300

    Fix ejercicio 4

---
+++ js/ejercicio-5.js
@@ -0,0 +1,8 @@
+/*
+5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+*/
+let numero1 = ("Ingrese el primer número");
+let numero2 = ("Ingrese el segundo número");
+document.writeln("La suma es: " + (numero1 + numero2));
